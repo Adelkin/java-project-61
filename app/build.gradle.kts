0618700f7ef1632e9afd5b88e45ef06c3ab6cc81
Adel Utyashev <adel99685@gmail.com>
2024-09-11 22:46:55 +0300

    Add instructions to clean project before build

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("application")
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
