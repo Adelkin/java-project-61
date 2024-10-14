90885ee4718303874cd13068ece20db507817ae6
Adel Utyashev <adel99685@gmail.com>
2024-10-14 23:02:49 +0300

    Add Even Game

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Adelkin/java-project-61/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Adelkin/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/b2f456f3a8bf1ba020a4/maintainability)](https://codeclimate.com/github/Adelkin/java-project-61/maintainability)
+
+#### Demo of Even Game
+[![asciicast](https://asciinema.org/a/0lKYgieIiKM2uzSFr3ozNAHai.svg)](https://asciinema.org/a/0lKYgieIiKM2uzSFr3ozNAHai)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,33 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetUser();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("1 - Greet");
+            System.out.println("2 - Even");
+            System.out.println("0 - Exit");
+            System.out.print("Your choice: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    Cli.greetUser();
+                    break;
+                case "2":
+                    EvenGame.start();
+                    break;
+                case "0":
+                    System.out.println("Goodbye!");
+                    return;  // Выход из программы
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
