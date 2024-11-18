e8f1ba00504717ac475fbcdab740055795666ec1
Adel Utyashev <adel99685@gmail.com>
2024-11-18 22:18:08 +0300

    fix magic numbers in Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class Even {
     private static final int ROUNDS = 3;
+    private static final int MAX_NUMBER = 100; // Константа для максимального числа
     private static final Random RANDOM = new Random();
 
     public static void start() {
@@ -12,7 +13,7 @@ public class Even {
         String[][] roundsData = new String[ROUNDS][2];
 
         for (int i = 0; i < ROUNDS; i++) {
-            int number = RANDOM.nextInt(100);
+            int number = RANDOM.nextInt(MAX_NUMBER);
             String correctAnswer = (number % 2 == 0) ? "yes" : "no";
             roundsData[i][0] = String.valueOf(number);
             roundsData[i][1] = correctAnswer;
