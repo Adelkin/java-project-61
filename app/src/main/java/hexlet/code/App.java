5ed4d40a268e158cc057a02156615acee64a518d
Adel Utyashev <adel99685@gmail.com>
2024-11-18 22:10:16 +0300

    fix magic numbers

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,41 +9,49 @@ import hexlet.code.games.Progression;
 import java.util.Scanner;
 
 public class App {
+    private static final int GREET = 1;
+    private static final int EVEN = 2;
+    private static final int CALC = 3;
+    private static final int GCDGAME = 4;
+    private static final int PROGRESSION = 5;
+    private static final int PRIME = 6;
+    private static final int EXIT = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
+        System.out.println(GREET + " - Greet");
+        System.out.println(EVEN + " - Even");
+        System.out.println(CALC + " - Calc");
+        System.out.println(GCDGAME + " - GCD");
+        System.out.println(PROGRESSION + " - Progression");
+        System.out.println(PRIME + " - Prime");
+        System.out.println(EXIT + " - Exit");
 
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
 
         switch (choice) {
-            case 1:
+            case GREET:
                 Cli.greet();
                 break;
-            case 2:
+            case EVEN:
                 Even.start();
                 break;
-            case 3:
+            case CALC:
                 Calc.start();
                 break;
-            case 4:
+            case GCDGAME:
                 GCD.start();
                 break;
-            case 5:
+            case PROGRESSION:
                 Progression.start();
                 break;
-            case 6:
+            case PRIME:
                 Prime.start();
                 break;
-            case 0:
+            case EXIT:
                 System.out.println("Goodbye!");
                 break;
             default:
