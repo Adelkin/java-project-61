9dc863949cc15b25ada96c469e610ba259317db8
Adel Utyashev <adel99685@gmail.com>
2024-11-18 21:55:55 +0300

    fix games

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,48 +11,46 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int choice;
 
-        do {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GCD");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
 
-            System.out.print("Your choice: ");
-            choice = scanner.nextInt();
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    Cli.greet();
-                    return;
-                case 2:
-                    Even.start();
-                    break;
-                case 3:
-                    Calc.start();
-                    break;
-                case 4:
-                    GCD.start();
-                    break;
-                case 5:
-                    Progression.start();
-                    break;
-                case 6:
-                    Prime.start();
-                    break;
-                case 0:
-                    System.out.println("Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid choice.");
-                    break;
-            }
+        switch (choice) {
+            case 1:
+                Cli.greet();
+                break;
+            case 2:
+                Even.start();
+                break;
+            case 3:
+                Calc.start();
+                break;
+            case 4:
+                GCD.start();
+                break;
+            case 5:
+                Progression.start();
+                break;
+            case 6:
+                Prime.start();
+                break;
+            case 0:
+                System.out.println("Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid choice. Exiting...");
+                break;
+        }
 
-        } while (choice != 0);
+        scanner.close();
     }
 }
